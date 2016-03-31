5c3690290ea67f38007c3a86255df3e557b7aa60
Ismail NGUYEN <me@nguyenismail.com>
2016-04-01 01:38:27 +0200

    dummy error page

---
+++ views/pages/error.php
@@ -0,0 +1,9 @@
+<div class="container">
+	<h3>Error</h3>
+	
+	<div class="row">
+		<p>An error has occurred ...</p>
+		<p class="m_2"><a href="/">Go back home</a></p>
+	</div>
+	<div class="clearfix"></div>
+</div>
