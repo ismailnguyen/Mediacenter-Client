d76f1d6e148eb53f7b0b1ff7b16e5c03d5804015
Ismail NGUYEN <me@nguyenismail.com>
2016-04-03 22:49:39 +0200

    mocked repositories

---
+++ repositories/mock/account_repository.php
@@ -0,0 +1,12 @@
+<?php
+	class MockAccountRepository {
+		
+		public function getAccount() {
+			$m = new Account();
+			$m->pseudo = "Ismail";
+			$m->token = "23454H3UBJHJ32T33AFL34593I4UV5NSTJG23V4B";
+			
+			return m;
+		}
+	}
+?>
