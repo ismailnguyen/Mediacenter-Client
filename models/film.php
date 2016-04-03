cf8e959b4883ce8035b947c45639655b84ad3197
Ismail NGUYEN <me@nguyenismail.com>
2016-04-03 22:49:15 +0200

    update models

--- models/film.php
+++ models/film.php
@@ -1,28 +1,12 @@
 <?php
 	class Film {
 		
-		public $name;
-		public $surname;
-		
-		public function __construct($name, $surname) {
-			$this->name = $name;
-			$this->surname = $surname;
-		}
-		
-		public function getName() {
-			return $name;
-		}
-		
-		public function setName($name) {
-			$this->name = $name;
-		}
-		
-		public function getSurname() {
-			return $surname;
-		}
-		
-		public function setSurname($surname) {
-			$this->surname = $surname;
-		}
+		public $uuid;
+		public $title;
+		public $director;
+		public $description;
+		public $publication;
+		public $image;
+		public $path;
 	}
 ?>
